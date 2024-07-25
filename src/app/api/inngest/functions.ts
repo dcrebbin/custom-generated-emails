7cd80d9830ef7a3631d2c8c3aa921c4087d833d3
Devon Crebbin <devon@artvuu.group>
2024-07-25 16:05:51 +1000

    added notification to inngest

--- src/app/api/inngest/functions.ts
+++ src/app/api/inngest/functions.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_EMAILS } from "~/app/constants/config";
 import { inngest } from "~/innjest/client";
 
 export const helloWorld = inngest.createFunction(
@@ -8,3 +9,79 @@ export const helloWorld = inngest.createFunction(
         return { event, body: "Hello, World!" };
     },
 );
+
+export const notification = inngest.createFunction(
+    { id: "notification" },
+    { event: "notification" },
+    async ({ event, step }) => {
+        console.log("Notification Starting");
+        for (const customEmail of CUSTOM_EMAILS) {
+            const today = new Date();
+            const dayOfWeek = today.getDay();
+            if (customEmail.schedule[dayOfWeek] === 1) {
+                await step.run("send-custom-email", async () => {
+                    await sendCustomEmail(customEmail.topic, customEmail.sendTo, customEmail.subject);
+                });
+            }
+        }
+        console.log("Notification Ending");
+        return { event, body: "Notification Ended" };
+    },
+);
+
+
+async function perplexity(query: string) {
+    console.log("Perplexity Request Starting");
+    const perplexity = await fetch(`${process.env.SERVER_URL}/api/perplexity`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": `${process.env.SERVER_PASSWORD}`,
+        },
+        body: JSON.stringify({
+            query: query,
+        }),
+    });
+    console.log("Perplexity Request Ending");
+    return perplexity;
+}
+
+async function openAi(query: string) {
+    console.log("OpenAI Request Starting");
+    const res = await fetch(`${process.env.SERVER_URL}/api/open-ai`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": `${process.env.SERVER_PASSWORD}`,
+        },
+        body: JSON.stringify({
+            query: query,
+        }),
+    });
+    console.log("OpenAI Request Ending");
+    return res;
+}
+
+async function sendCustomEmail(query: string, email: string, subject: string) {
+    console.log("Send Custom Email Starting");
+    const res = process.env.USE_OPEN_AI == "true" ? await openAi(query) : await perplexity(query);
+    const data = await res.text();
+    const emailSent = await fetch(`${process.env.SERVER_URL}/api/email`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-api-key": `${process.env.SERVER_PASSWORD}`,
+        },
+        body: JSON.stringify({
+            email: email,
+            subject: subject,
+            text: data,
+        }),
+    });
+    const emailSentData = await emailSent.text();
+    console.log("Send Custom Email Ending");
+    console.log(emailSentData);
+    return new Response(data, {
+        status: 200,
+    });
+}
--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,11 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "~/innjest/client";
-import { helloWorld } from "./functions";
+import { helloWorld, notification } from "./functions";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
-        helloWorld
+        helloWorld,
+        notification,
     ],
 });
