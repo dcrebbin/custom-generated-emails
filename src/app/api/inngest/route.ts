c9d508be34afaebe514bb14086e8e7cff187fbaf
Devon Crebbin <devon@artvuu.group>
2024-07-25 16:48:34 +1000

    added streaming to inngest

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -5,6 +5,7 @@ import { helloWorld, notification } from "./functions";
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
     client: inngest,
+    streaming: "allow",
     signingKey: process.env.INNGEST_SIGNING_KEY,
     functions: [
         helloWorld,
